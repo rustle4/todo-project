59467f1a6e571c24b07e0f7c0e9a510ffabff60b
rustle <shelterr0103@gmail.com>
2026-09-01 15:16:51 +0200

    Disable prepared statement cache for asyncpg with PgBouncer

--- app/core/database.py
+++ app/core/database.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.DATABASE_URL)
+engine = create_async_engine(
+    settings.DATABASE_URL, echo=True, connect_args={"statement_cache_size": 0}
+)
 
 AsyncSessionLocal = async_sessionmaker(
     engine,
